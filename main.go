52405978fe04eb300743e7bae02f7f9b04a4706c
josephmusya <muciajoe@gmail.com>
2025-05-01 16:46:10 +0300

    Update MQTT Subscription topics

--- main.go
+++ main.go
@@ -13,7 +13,6 @@ import (
 	"os/signal"
 	"path/filepath"
 	"regexp"
-	"strconv"
 	"strings"
 	"syscall"
 	"time"
@@ -437,7 +436,190 @@ func updateTankMessages(tankID string, newMessage Message) {
 	}
 }
 
-func checkValForNotifcation(val float64, tankID string, sensorId string) {
+// func checkValForNotifcation(val float64, tankID string, sensorId string) {
+
+// 	fmt.Println("VALUE: ", val)
+// 	// Send a GET request to localhost/devices/tankID
+// 	req, err := http.NewRequest(http.MethodGet, fmt.Sprintf("http://localhost/devices/%s", tankID), nil)
+// 	if err != nil {
+// 		fmt.Println("Error creating request:", err)
+// 		return
+// 	}
+
+// 	// Set the Content-Type and Accept headers
+// 	req.Header.Set("Content-Type", "application/json")
+// 	req.Header.Set("Accept", "application/json")
+
+// 	// Send the request
+// 	client := http.DefaultClient
+// 	resp, err := client.Do(req)
+// 	if err != nil {
+// 		fmt.Println("Error sending request:", err)
+// 		return
+// 	}
+// 	defer resp.Body.Close()
+
+// 	// Read the response body
+// 	body, err := ioutil.ReadAll(resp.Body)
+// 	if err != nil {
+// 		fmt.Println("Error reading response body:", err)
+// 		return
+// 	}
+
+// 	// Unmarshal the JSON data into a Tank struct
+// 	var tank Tank
+// 	err = json.Unmarshal(body, &tank)
+// 	if err != nil {
+// 		fmt.Println("Error unmarshaling tank:", err)
+// 		return
+// 	}
+
+// 	liters := 0.0
+
+// 	tankHeight := tank.Meta.Settings.Height
+// 	tankOffset := tank.Meta.Settings.Offset
+// 	tankCapacity := tank.Meta.Settings.Capacity
+
+// 	calculatedValue := ((tankHeight - (val - tankOffset)) / tankHeight) * tankCapacity
+	
+// 	liters = float64(calculatedValue)
+
+// 	percentage := (liters/tankCapacity)*100
+
+// 	date := time.Now().Add(3*time.Hour).Format("2006-01-02 15:04:05")
+
+// 	// Send a GET request to localhost/devices/tankID/sensors
+// 	resp, err = http.Get(fmt.Sprintf("http://localhost/devices/%s/sensors/%s", tankID, sensorId))
+// 	if err != nil {
+// 		fmt.Println("Error retrieving sensors:", err)
+// 		return
+// 	}
+
+// 	defer resp.Body.Close()
+
+// 	// Read the response body
+// 	sensorBody, err := ioutil.ReadAll(resp.Body)
+// 	if err != nil {
+// 		fmt.Println("Error reading sensor response body:", err)
+// 		return
+// 	}
+
+// 	// Unmarshal the sensor data into a slice of SensorData
+// 	var sensor SensorData
+// 	err = json.Unmarshal(sensorBody, &sensor)
+// 	if err != nil {
+// 		fmt.Println("Error unmarshaling sensors:", err)
+// 		return
+// 	}
+
+// 	lowerLimit := sensor.Meta.CriticalMin
+// 	upperLimit := sensor.Meta.CriticalMax
+
+// 	tokens := getTokens()
+// 	phone := getPhone()
+
+
+// 	fmt.Println(percentage)
+
+// 	// var difference float64 = percentage - prevValue;
+	
+// 	// sending alerts during max and min alerts 
+// 	if percentage <= lowerLimit && !notified  {
+// 		fmt.Println("Sending LOW notification")
+// 		title := fmt.Sprintf("%s is almost empty", tank.Name)
+// 		body := fmt.Sprintf("Water level for %s is at %d%%", tank.Name, int(percentage))
+// 		for _, token := range tokens {
+// 			sendPushNotification(token, title, body)
+// 		}
+
+// 		message := Message{
+// 			TankName: 	tank.Name,
+// 			Message: 	body,
+// 			Date:    	date,
+// 		}
+// 		updateTankMessages(tank.ID, message)
+
+// 		// phone := string(0)
+// 		// sendMessage(body, phone)
+// 		notified = true
+// 		return
+// 	} else if percentage >= upperLimit && !notified {
+// 		fmt.Println("Sending HIGH notification")
+// 		title := fmt.Sprintf("%s is almost filled", tank.Name)
+// 		body := fmt.Sprintf("Water level for %s is at %d%%", tank.Name, int(percentage))
+		
+// 		for _, token := range tokens {
+// 			sendPushNotification(token, title, body)
+// 		}
+
+// 		message := Message{
+// 			TankName: tank.Name,
+// 			Message: body,
+// 			Date:    	date,
+// 		}
+// 		updateTankMessages(tank.ID, message)
+
+// 		// phone := string(0)
+// 		// sendMessage(body, phone)
+// 		notified = true
+// 		return
+// 	} else if (percentage > lowerLimit && percentage < upperLimit ) {
+// 		notified = false
+// 		return
+// 	}
+	
+// 	// Send alerts at extreme levels
+// 	if percentage >= float64(tankFull) && !criticalLevelNotified  {
+// 		title := fmt.Sprintf("%s is already full", tank.Name)
+// 		body := fmt.Sprintf("Water level for %s is at %d%%. Turn off the actuator.", tank.Name, int(percentage))
+		
+// 		for _, token := range tokens {
+// 			sendPushNotification(token, title, body)
+// 		}
+
+// 		sendMessage(body, phone)
+
+// 		message := Message{
+// 			TankName: tank.Name,
+// 			Message: body,
+// 			Date:    	date,
+// 		}
+// 		updateTankMessages(tank.ID, message)
+
+// 		// phone := string(0)
+// 		// sendMessage(body, phone)
+// 		criticalLevelNotified = true
+// 		return
+// 	} else if percentage <= float64(tankEmpty) && !criticalLevelNotified {
+// 		title := fmt.Sprintf("%s is running dry", tank.Name)
+// 		body := fmt.Sprintf("Water level for %s is at %d%%. Turn on the actuators", tank.Name, int(percentage))
+		
+// 		for _, token := range tokens {
+// 			sendPushNotification(token, title, body)
+// 		}
+		
+// 		sendMessage(body, phone)
+
+// 		message := Message{
+// 			TankName: tank.Name,
+// 			Message: body,
+// 			Date:    	date,
+// 		}
+// 		updateTankMessages(tank.ID, message)
+
+// 		// phone := string(0)
+// 		// sendMessage(body, phone)
+// 		criticalLevelNotified = true
+// 		return
+// 	} else if (percentage > float64(tankEmpty) && percentage < float64(tankFull) ) {
+// 		criticalLevelNotified = false
+// 		return
+// 	}
+
+// }
+
+
+func checkValForNotifcation(tankID string) {
 
 	// Send a GET request to localhost/devices/tankID
 	req, err := http.NewRequest(http.MethodGet, fmt.Sprintf("http://localhost/devices/%s", tankID), nil)
@@ -480,6 +662,39 @@ func checkValForNotifcation(val float64, tankID string, sensorId string) {
 	tankOffset := tank.Meta.Settings.Offset
 	tankCapacity := tank.Meta.Settings.Capacity
 
+	var val float64
+	var lowerLimit, upperLimit float64
+	found := false
+	
+	for _, sensor := range tank.Sensors {
+		if sensor.Meta.Kind == "WaterLevel" {
+			// Get value
+			switch v := sensor.Value.(type) {
+			case float64:
+				val = v
+			case int:
+				val = float64(v)
+			case json.Number:
+				val, _ = v.Float64()
+			default:
+				fmt.Println("Unsupported sensor value type")
+				return
+			}
+	
+			// Capture sensor ID and critical limits
+			lowerLimit = sensor.Meta.CriticalMin
+			upperLimit = sensor.Meta.CriticalMax
+	
+			found = true
+			break
+		}
+	}
+
+	if !found {
+		fmt.Println("WaterLevel sensor not found")
+		return
+	}
+
 	calculatedValue := ((tankHeight - (val - tankOffset)) / tankHeight) * tankCapacity
 	
 	liters = float64(calculatedValue)
@@ -487,33 +702,6 @@ func checkValForNotifcation(val float64, tankID string, sensorId string) {
 	percentage := (liters/tankCapacity)*100
 
 	date := time.Now().Add(3*time.Hour).Format("2006-01-02 15:04:05")
-
-	// Send a GET request to localhost/devices/tankID/sensors
-	resp, err = http.Get(fmt.Sprintf("http://localhost/devices/%s/sensors/%s", tankID, sensorId))
-	if err != nil {
-		fmt.Println("Error retrieving sensors:", err)
-		return
-	}
-
-	defer resp.Body.Close()
-
-	// Read the response body
-	sensorBody, err := ioutil.ReadAll(resp.Body)
-	if err != nil {
-		fmt.Println("Error reading sensor response body:", err)
-		return
-	}
-
-	// Unmarshal the sensor data into a slice of SensorData
-	var sensor SensorData
-	err = json.Unmarshal(sensorBody, &sensor)
-	if err != nil {
-		fmt.Println("Error unmarshaling sensors:", err)
-		return
-	}
-
-	lowerLimit := sensor.Meta.CriticalMin
-	upperLimit := sensor.Meta.CriticalMax
 
 	tokens := getTokens()
 	phone := getPhone()
@@ -617,34 +805,54 @@ func checkValForNotifcation(val float64, tankID string, sensorId string) {
 
 // initialize checking for device when it goes offline ( a boolean variable for online, true by default)
 
+// var messagePubHandler mqtt.MessageHandler = func(client mqtt.Client, msg mqtt.Message) {
+
+// 	// start timeout for 10 minutes after this function executes
+// 	// reset timout before 10 minutes if a message is received (online status to true)
+// 	// if timeout resets before 10 minutes end do nothing
+// 	// if timeout executes upto 10 mins, create a print message offlinev (online status to false)
+// 	// when a message is received, toggle the online status to false
+	
+// 	// regex := regexp.MustCompile(`^devices/([^/]+)/sensors/([^/]+)/value$`)
+// 	regex := regexp.MustCompile(`^devices/([^/]+)$`)
+
+// 	matches := regex.FindStringSubmatch(msg.Topic())
+
+// 	val := string(msg.Payload())
+
+// 	fmt.Println("VAL -> ",val);
+
+// 	floatVal, err := strconv.ParseFloat(val, 64)
+
+// 	if err != nil {
+// 		fmt.Println("Error parsing float:", err)
+// 		return
+// 	}
+
+// 	if len(matches) >= 3 {
+// 		deviceID := matches[1]
+// 		sensorID := matches[2]
+
+// 		checkValForNotifcation( floatVal, deviceID, sensorID)
+// 	}
+
+// }
+
 var messagePubHandler mqtt.MessageHandler = func(client mqtt.Client, msg mqtt.Message) {
 
-	// start timeout for 10 minutes after this function executes
-	// reset timout before 10 minutes if a message is received (online status to true)
-	// if timeout resets before 10 minutes end do nothing
-	// if timeout executes upto 10 mins, create a print message offlinev (online status to false)
-	// when a message is received, toggle the online status to false
-	
-	regex := regexp.MustCompile(`^devices/([^/]+)/sensors/([^/]+)/value$`)
+	regex := regexp.MustCompile(`^devices/([^/]+)$`)
 	matches := regex.FindStringSubmatch(msg.Topic())
 
-	val := string(msg.Payload())
 
-	floatVal, err := strconv.ParseFloat(val, 64)
 
-	if err != nil {
-		fmt.Println("Error parsing float:", err)
-		return
-	}
-
-	if len(matches) >= 3 {
+	if len(matches) >= 2 {
 		deviceID := matches[1]
-		sensorID := matches[2]
-
-		checkValForNotifcation( floatVal, deviceID, sensorID)
+		checkValForNotifcation(deviceID)
+	} else {
+		fmt.Println("No match for topic")
 	}
-
 }
+
 
 var connectHandler mqtt.OnConnectHandler = func(client mqtt.Client) {
 	fmt.Printf("[ MQTT ] Connected\n")
@@ -742,13 +950,25 @@ func getMqttTopics () []MqttTopic {
 	// Filter the sensors based on tankID and kind = "WaterLevel" in the meta field
 	var topics []MqttTopic
 
+	// for _, tank := range tanks {
+	// 	for _, sensor := range tank.Sensors {
+	// 		if sensor.Meta.Kind == "WaterLevel" {
+	// 			topic := MqttTopic{
+	// 				TopicId: fmt.Sprintf("devices/%s/sensors/%s/#", tank.ID, sensor.ID),
+	// 			}
+
+	// 			topics = append(topics, topic)
+	// 		}
+	// 	}
+		
+	// }
+
 	for _, tank := range tanks {
 		for _, sensor := range tank.Sensors {
 			if sensor.Meta.Kind == "WaterLevel" {
 				topic := MqttTopic{
-					TopicId: fmt.Sprintf("devices/%s/sensors/%s/#", tank.ID, sensor.ID),
+					TopicId: fmt.Sprintf("devices/%s/#", tank.ID),
 				}
-
 				topics = append(topics, topic)
 			}
 		}
